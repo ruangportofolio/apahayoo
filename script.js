83368a2e793c277595ef2504af8781bc921c3ada
ruangportofolio <orbitalportofolio@gmail.com>
2025-12-05 08:57:40 +0700

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const bgm = document.getElementById("bgm");
-const playBtn = document.getElementById("playBtn");
-
-playBtn.addEventListener("click", () => {
-    bgm.volume = 1;
-    bgm.play();
-    playBtn.style.animation = "none"; // stop wiggle
-    playBtn.style.opacity = "0.4"; // tampak sudah ditekan
-});
